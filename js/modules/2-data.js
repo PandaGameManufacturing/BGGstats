064326662090e8700d7e7e78b31239363dfd0ff8
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-03 20:52:01 -0500

    navbar to grey

---
+++ js/modules/2-data.js
@@ -0,0 +1,14 @@
+
+/**********************\
+|  2. Data             |
+\**********************/
+
+var thing;
+
+  for (var i = 0; i < 3; i++){
+    console.log("do thing");
+    var thing = "that's what";
+
+  }
+
+  var thing = "hi";
