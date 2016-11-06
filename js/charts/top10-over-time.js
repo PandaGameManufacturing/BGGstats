a19cc3106ebd8fe6a81c74bafc63ddabdf7b5a05
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-06 10:49:21 -0600

    top 10 chart colors and thickness changed

--- js/charts/top10-over-time.js
+++ js/charts/top10-over-time.js
@@ -2,61 +2,75 @@
 |  Top 10              |
 \**********************/
 
-     google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable ([
-            ['Year', 'Pandemic Legacy: Season 1',                     // 1
-                     'Through the Ages: A New Story of Civilization', // 2
-                     'Twilight Struggle',                             // 3
-                     'Terra Mystica',
-                     'Caverna: The Cave Farmers',
-                     'Star Wars: Rebellion',
-                     'Puerto Rico',
-                     '7 Wonders Duel',
-                     'The Castles of Burgundy',
-                     'Agricola'
-            ],
-            ['',      11,  11, 4,  11,  11,  11,  11,  11,  11,  1],     // dummy row
-            ['2009',  11,  7,  4,  11,  11,  11,  11,  11,  11,  1],
-            ['2010',  11,  5,  4,  11,  11,  11,  11,  11,  11,  1],
-            ['2011',  11,  4,  1,  11,  11,  11,  11,  11,  11,  3],
-            ['2012',  11,  4,  1,  11,  11,  11,  11,  11,  11,  2],
-            ['2013',  11,  2,  1,  11,  11,  11,  11,  11,  11,  3],
-            ['2014',  11,  2,  1,  6,   11,  11,  11,  11,  11,  3],
-            ['2015',  11,  3,  1,  2,    6,  11,  11,  11,  11,  4],
-            ['2016',  1,   2,  3,  4,    5,  6,    7,  11,  9,  10],
-            ['',      1,   2,  3,  4,    5,  6,    7,  8,   9,  10]      // dummy row
-          ]);
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable ([
+    ['Year', 'Pandemic Legacy: Season 1',                     // 1
+             'Through the Ages: A New Story of Civilization', // 2
+             'Twilight Struggle',                             // 3
+             'Terra Mystica',                                 // 4
+             'Caverna: The Cave Farmers',                     // 5
+             'Star Wars: Rebellion',                          // 6
+             'Puerto Rico',                                   // 7
+             '7 Wonders Duel',                                // 8
+             'The Castles of Burgundy',                       // 9
+             'Agricola',                                     // 10
 
-        var options = {
-          colors: ['#e0440e', '#5DA5DA', '#FAA43A', '#60BD68', '#F17CB0', '#B2912F', '#70dde5', '#B276B2', '#DECF3F', '#685e43'],
-          legend: 'none',
-          curveType: 'function',
-          pointSize: 0,
-          lineWidth: 2,
-          chartArea: { width: "100%", height: "80%"},
-          vAxis:{
-           baselineColor: '#fff',
-           gridlineColor: '#fff',
-           textPosition: 'none',
-           direction: -1,
-           viewWindow: { min: 0, max: 10}
-          },
-          hAxis:{
-           baselineColor: '#fff',
-          }
-        };
+             'Power Grid',                                   // 11
+             'Tigris & Euphrates',                           // 12
+             'Dominion',                                     // 13
+             'El Grande',                                    // 14
+             'Caylus',                                       // 15
+             'Race for the Galaxy',                          // 16
+             'Le Havre',                                     // 17
+             'Dominion: Intrigue',                           // 18
+             'Brass',                                        // 19
+             'Eclipse',                                      // 20
+             'Android: Netrunner',                           // 21
+             'Mage Knight Board Game'                        // 22
+    ],
+    //       1    2  3   4   5   6   7   8   9  10    11  12  13  14  15  16  17  18  19  20  21  22
+    // ['',     11, 11, 4, 11, 11, 11,  2, 11, 11,  1,    3,  5,  6,  8, 09, 10, 11, 11, 11, 11, 11, 11],
+    ['2009', 11,  7, 4, 11, 11, 11,  2, 11, 11,  1,    3,  5,  6,  8, 09, 10, 11, 11, 11, 11, 11, 11],
+    ['2010', 11,  5, 4, 11, 11, 11,  2, 11, 11,  1,    3,  8,  6,  9, 10, 11,  7, 11, 11, 11, 11, 11],
+    ['2011', 11,  4, 1, 11, 11, 11,  2, 11, 11,  3,    5, 11,  8, 11, 10, 11,  6,  7,  9, 11, 11, 11],
+    ['2012', 11,  4, 1, 11, 11, 11,  3, 11, 11,  2,    5, 11,  9, 11, 10, 11,  6,  8,  7, 11, 11, 11],
+    ['2013', 11,  2, 1, 11, 11, 11,  4, 11, 11,  3,    6, 11, 11, 11, 11, 11,  7, 11, 10,  5,  8,  9],
+    ['2014', 11,  2, 1,  6, 11, 11,  4, 11, 11,  3,    8, 11, 11, 11, 11, 11, 10, 11, 11,  7,  5,  9],
+    ['2015', 11,  3, 1,  2,  6, 11,  5, 11, 11,  4,   10, 11, 11, 11, 11, 11, 11, 11, 11,  8,  7,  9],
+    ['2016',  1,  2, 3,  4,  5,  6,  7, 11,  9, 10,   11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,  9],
+    ['',      1,  2, 3,  4,  5,  6,  7,  8,  9, 10,   11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11]
+  ]);
 
-        var chart = new google.visualization.LineChart(document.getElementById('top10'));
-        chart.draw(data, options);
-
-        var chart1 = "done";
-
-        $(window).resize(function() {
-          if(chart1=="done"){
-          chart1 = "waiting";
-        setTimeout(function(){drawChart();chart1 = "done"},1000); // only resize every second
-        }
-        });
+  var options = {
+    colors: ['#e0440e', "#D62896", "#B728A1", "#9928AC", "#7A28B7", "#5C29C2", "#554FC4", "#4F75C7", "#499BC9", "#43C1CC", '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee', '#ccc', '#eee'],
+    legend: 'none',
+    curveType: 'function',
+    pointSize: 0,
+    lineWidth: 1.5,
+    chartArea: { width: "100%", height: "80%"},
+    vAxis:{
+     baselineColor: '#fff',
+     gridlineColor: '#fff',
+     textPosition: 'none',
+     direction: -1,
+     viewWindow: { min: 0, max: 11}
+    },
+    hAxis:{
+      gridlines: { color: '#f3f3f3', count: 8}
     }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('top10'));
+  chart.draw(data, options);
+
+  var chart1 = "done";
+
+  $(window).resize(function() {
+    if(chart1=="done"){
+    chart1 = "waiting";
+  setTimeout(function(){drawChart();chart1 = "done"},1000); // only resize every second
+  }
+  });
+}
