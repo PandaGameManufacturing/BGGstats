cfb404bb4d3dbc64460f420368269d0cfdf1192e
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-07 12:52:24 -0600

    get hotness data

--- js/modules/2-data.js
+++ js/modules/2-data.js
@@ -3,3 +3,21 @@
 |  2. Data             |
 \**********************/
 
+function getHotness () {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://boardgamegeek.com/xmlapi2/hot?boardgame"
+    }).done(data => {
+      resolve(data);
+    }).fail(error => {
+      reject(error);
+    });
+  });
+}
+
+getHotness()
+.then( data => {
+  console.log("Hotness Data:", data);
+}, error => {
+  console.log("error:", error);
+});
