2f72ede2c8f1732a4b2f29022f02b02c5518a102
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-08 16:01:56 -0600

    5 games from hotness data saved in object

--- javascripts/data/bgg-api/game-details-call.js
+++ javascripts/data/bgg-api/game-details-call.js
@@ -1,1 +1,8 @@
 "use strict";
+let getData = require("../get-data");
+
+let gameDetails = function(id) {
+  return getData(`http://bgg-api.herokuapp.com/api/v1/thing?id=${id}`);
+};
+
+module.exports = gameDetails;
