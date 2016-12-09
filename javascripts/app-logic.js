b08d95ec83c3268ebf1cd76d5d0321abdc1ffc3a
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-09 11:40:50 -0600

    reached mvp. first chart loads

--- javascripts/app-logic.js
+++ javascripts/app-logic.js
@@ -7,3 +7,4 @@ require("./chart-visuals/chart-visuals-loader");
 let loadChart = require("./chart-logic/chart-logic-loader");
 
 loadChart.hotness("slot1");
+// loadChart.top10("slot2");
--- javascripts/chart-visuals/top10-chart.js
+++ javascripts/chart-visuals/top10-chart.js
@@ -1,42 +1,53 @@
 "use strict";
 
 let $ = require("../../node_modules/jquery/src/jquery.js");
-require("../../third-party-js/google-charts-loader");
 
- // <div class="row">
+let top10Chart = (title, slot) => {
 
- //          <!-- Top 10  -->
- //          <div class="col-sm-12 col-md-12 col-lg-12">
- //          <div class="statbox">
- //                          <div class="label-title">
- //              <h2>Top 10</h2>
- //              <a><img class="help pull-right" src="/dist/images/icons/help.svg" alt="What is The Hotness Stat?"></a>
- //            </div>
+  let snippets = `
+    <div class="row">
 
- //                <div class="col-sm-12 col-md-7 col-lg-8">
+      <!-- Top 10  -->
+      <div class="col-sm-12 col-md-12 col-lg-12">
+      <div class="statbox">
+                      <div class="label-title">
+          <h2>${title}</h2>
+          <a><img class="help pull-right" src="/dist/images/icons/help.svg" alt="What is The Hotness Stat?"></a>
+        </div>
 
- //                  <!-- Line Chart -->
- //                 <div id="top10"></div>
+            <div class="col-sm-12 col-md-7 col-lg-8">
 
- //                </div>
+              <!-- Line Chart -->
+             <div id="top10"></div>
 
- //                <!-- Table -->
- //                <div class="col-sm-12 col-md-5 col-lg-4">
- //                  <ol class="color-list">
- //                    <li>Pandemic Legacy: Season 1</li>
- //                    <li>Through the Ages: A New Story of Civilization</li>
- //                    <li>Twilight Struggle</li>
- //                    <li>Terra Mystica</li>
- //                    <li>Caverna: The Cave Farmers</li>
- //                    <li>Star Wars: Rebellion</li>
- //                    <li>Puerto Rico</li>
- //                    <li>7 Wonders Duel</li>
- //                    <li>The Castles of Burgundy</li>
- //                    <li>Agricola</li>
- //                 </ol>
- //                </div>
+            </div>
 
- //              </div>
- //          </div>
- //        </div>
+            <!-- Table -->
+            <div class="col-sm-12 col-md-5 col-lg-4">
+              <ol class="color-list">
+                <li>Pandemic Legacy: Season 1</li>
+                <li>Through the Ages: A New Story of Civilization</li>
+                <li>Twilight Struggle</li>
+                <li>Terra Mystica</li>
+                <li>Caverna: The Cave Farmers</li>
+                <li>Star Wars: Rebellion</li>
+                <li>Puerto Rico</li>
+                <li>7 Wonders Duel</li>
+                <li>The Castles of Burgundy</li>
+                <li>Agricola</li>
+             </ol>
+            </div>
+
+          </div>
+      </div>
+    </div>
+
+  `;
+
+  $(`#${slot}`).html(snippets);
+
+};
+
+module.exports = top10Chart;
+
 
