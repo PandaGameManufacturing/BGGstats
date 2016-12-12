963ed17efae737eb231e67d9e2370accd8c5bd3c
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-12 12:06:32 -0600

    using wrong variable

--- javascripts/push-data/push-to-GameRank.js
+++ javascripts/push-data/push-to-GameRank.js
@@ -4,35 +4,13 @@ function pushToGameRank(rankObject) {
   console.log("pushToGameRank function runs");
   console.log("Object in function:", rankObject);
 
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('POST', 'https://bggstats-2de27.firebaseio.com/GameRank.json');
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  var req = new XMLHttpRequest();
+  req.open('POST', 'https://bggstats-2de27.firebaseio.com/GameRank.json');
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    req.onload = function() {
-      console.log("on load runs");
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(req.response);
-      }
-      else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
-      }
-    };
-
-    // Handle network errors
-    req.onerror = function() {
-      console.log("on error runs");
-      reject(Error("Network Error"));
-    };
-
-    // send request
-    xmlhttp.send(JSON.stringify(rankObject));
-  });
+  // send request
+  req.send(JSON.stringify(rankObject));
+  console.log("request should have been sent on the line above");
 }
 
 module.exports = pushToGameRank;
