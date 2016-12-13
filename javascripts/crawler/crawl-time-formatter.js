ac7a8cd5b42c3aec5b43d3b78d8dbc40681f3cf2
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-13 09:41:59 -0600

    pulled data formatting out as own function

---
+++ javascripts/crawler/crawl-time-formatter.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let getPrettyDate = require("../assets/get-date");
+
+ // CRAWL TIME FORMATS
+  // CrawlYear         YYYY as number
+  // CrawlYMD          YYYYMMDD as string
+  // CrawlISO          ISO 8601 format
+  // CrawlLocal        human-readable army time date and time
+  // CrawlMilliseconds milliseconds since 1970
+
+let addCrawlTimes = function(object) {
+  let date = new Date();
+  object.CrawlISO = date;
+  object.CrawlLocal = date.toTimeString();
+  object.CrawlYMD = getPrettyDate();
+  object.CrawlYear = date.getFullYear();
+  object.CrawlMilliseconds = date.getTime();
+};
+
+module.exports = addCrawlTimes;
