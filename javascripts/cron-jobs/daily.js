b120199ddccb4d11b527abc0411d2e3d5ac14d6b
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-15 09:13:17 -0600

    missing semicoloin, removed old console log

--- javascripts/cron-jobs/daily.js
+++ javascripts/cron-jobs/daily.js
@@ -2,3 +2,6 @@
 
 // a cron job triggers this file at 00:05 every day. it runs:
 require("../crawler/rankings-crawler");
+
+//   manually run it with:
+//   `heroku run "node javascripts/cron-jobs/daily.js" --app bggstats`
