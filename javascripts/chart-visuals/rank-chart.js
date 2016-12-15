3b7525158d52071d72f8fb3a01dc25c339e78df5
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-15 15:44:48 -0600

    top game has big arrow

--- javascripts/chart-visuals/rank-chart.js
+++ javascripts/chart-visuals/rank-chart.js
@@ -5,9 +5,14 @@ let $ = require("jquery");
 let drawRankChart = (title, data, slot) => {
   console.log("movement chart data:", data);
 
+  let item1Link = `https://boardgamegeek.com/boardgame/${data[0].bgg}`;
+  let item1Rank = data[0].movement;
+  let item1ImageURL = data[0].details.thumbnail[0];
+  let biggestMover = `<ol><li><strong><a href="${item1Link}/">${data[0].name}</a></strong></li></ol>`;
+
   let snippets = `
-    <!-- Rank Chart  -->
-    <div class="row">
+<!-- Rank Chart  -->
+    <div class="row rankChart">
       <div class="col-sm-12 col-md-12 col-lg-12">
 
       <div class="statbox">
@@ -24,7 +29,26 @@ let drawRankChart = (title, data, slot) => {
 
             <!-- Details About Biggest Mover -->
             <div class="col-lg-4">
-              <p>details about biggest mover</p>
+
+              <div class="row">
+                ${biggestMover}
+              </div>
+
+              <div class="row">
+
+                <div class="col-sm-6">
+                  <img src="${item1ImageURL}" alt="${data[0].name}"/>
+                </div>
+
+                <div class="col-sm-6">
+
+                  <div id="rankMovement">${item1Rank}</div>
+                  <p id="rankDescription">up ${item1Rank} spots <br/>in a week</p>
+
+                </div>
+
+              </div>
+
             </div>
 
         </div>
