8564617cc5dff1ab3b7872668711fb21dbb0349f
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-18 13:56:33 -0600

    moving sass to load via javascript

---
+++ javascripts/get-data/data-template.js
@@ -0,0 +1,19 @@
+// This is a resource to help me keep track of how data is stored in my database
+
+"use strict";
+
+let Charts = 2;
+let Games = 2;
+let Rankings = 2;
+let GamesPublished = 2;
+let BGG = 2;
+let Users = 2;
+
+console.log("Data Template", {
+  "Charts": Charts,
+  "Games": Games,
+  "Rankings": Rankings,
+  "GamesPublished": GamesPublished,
+  "BGG": BGG,
+  "Users": Users
+});
