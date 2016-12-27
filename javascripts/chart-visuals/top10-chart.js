f99bcf5c7fd0bf023a8b1ea7b224759f008b956d
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-26 20:22:13 -0500

    top 10 line chart displays with old data

--- javascripts/chart-visuals/top10-chart.js
+++ javascripts/chart-visuals/top10-chart.js
@@ -1,16 +1,83 @@
 "use strict";
 
-let $ = require("jquery");
+let $ = require("jquery"),
+    chartLoader = require("../assets/google-charts-loader"),
+    getData = require("../get-data/get-data-loader");
 
-let drawTop10List = (title, crawlerData, slot) => {
+let drawTop10List = (title, data, slot) => {
+
+  console.log("data in top10:", data.charts.top10);
+  let top10List = data.charts.top10;
+
+    let // color scheme for line chart
+    n1  = "#26BB5D",
+    n2  = "#259E7C",
+    n3  = "#24819B",
+    n4  = "#2364BA",
+    n5  = "#2347D9",
+    n6  = "#503FC1",
+    n7  = "#7E37A9",
+    n8  = "#AB2F91",
+    n9  = "#D9277A",
+    n10 = "#D9274B",
+    x1  = "#ccc",
+    x2  = "#eee";
+
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+
+
+    function drawChart() {
+
+      let chartData = google.visualization.arrayToDataTable(getData.historic);
+
+      let options = {
+        colors: [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2, x1, x2],
+        legend: 'none',
+        curveType: 'function',
+        pointSize: 0,
+        lineWidth: 1.5,
+        chartArea: { width: "100%", height: "80%"},
+        vAxis:{
+         baselineColor: '#fff',
+         gridlineColor: '#fff',
+         textPosition: 'none',
+         direction: -1,
+         viewWindow: { min: 0, max: 11}
+        },
+        hAxis:{
+          gridlines: { color: '#f3f3f3', count: 8}
+        }
+      };
+
+      console.log("getData.historic:", getData.historic);
+
+      // display chart in id "top10"
+      let chart = new google.visualization.LineChart(document.getElementById('top10'));
+
+      // draw google chart
+      chart.draw(chartData, options);
+
+      // resize line chart if user changes width of page
+      let chart1 = "done";
+      $(window).resize(function() {
+        if(chart1=="done") {
+        chart1 = "waiting";
+      setTimeout( ()=> {
+        drawChart();chart1 = "done";
+      },500); // delay in milliseconds
+      }
+      });
+
+    }
 
   // build top 10 list
-  let top10html = "",
-      d = crawlerData;
+  let top10html = "";
 
   // loop over array of objects
-  for (let i = 0; i < crawlerData.length; i++) {
-    top10html += `<li><a href="http://boardgamegeek.com/boardgame/${d[i].BggId}/">${d[i].Name}<a/></li>`;
+  for (let i = 0; i < top10List.length; i++) {
+    console.log("top10List[i]:", top10List[i]);
+    top10html += `<li><a href="http://boardgamegeek.com/boardgame/${top10List[i]}/">${data.games[top10List[i]].name}<a/></li>`;
   }
 
   let snippets = `
