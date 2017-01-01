63887dce7507b660d57f01d90cfc860e36f3f37f
Nathan T. Baker <nathantbaker@gmail.com>
2016-12-31 22:48:23 -0600

    recrawl at page 72, remove dependance on getting game details

--- javascripts/data-manipulation/format-crawl-data.js
+++ javascripts/data-manipulation/format-crawl-data.js
@@ -10,7 +10,6 @@ let getDateMinus = require("../assets/get-date"),
     pushData = require("../push-data/push-data-serverside"),
     getCrawlTimes = require("../crawler/crawler-logic/crawl-time-formatter");
 
-
 // configuration options
 let today          = getDateMinus(0),
     compareString  = "yesterday",     // for console logs
@@ -26,7 +25,6 @@ let formatCrawlData = () => {
   console.log("");
 
   let dataTodayPromise = getTodayData(today);
-  console.log("dataTodayPromise:", dataTodayPromise);
   let dataComparePromise = getCompareData(compareString, compareDate, fallbackString, fallbackDate);
 
   // pull down the data I need
@@ -44,7 +42,9 @@ let formatCrawlData = () => {
     // build an array of movement from two sets of data
     // push up movement data
     let movement = calculateMovement.dayChange(data[0], data[1]);
+
     return movement;
+
 
   }).then( data => {
 
@@ -53,19 +53,18 @@ let formatCrawlData = () => {
     console.log(`::    - Lowest mover is down ${data[14].movementDay} (bggID: ${data[14].bggID})`);
 
     // make API calls for the 15 games and push up game details
-    let arrayOfGames = [];
-    for (let i = 0; i < data.length; i++) {
-      arrayOfGames.push(data[i].bggID);
-    }
+    // let arrayOfGames = [];
+    // for (let i = 0; i < data.length; i++) {
+    //   arrayOfGames.push(data[i].bggID);
+    // }
 
     // push up game details for movement games
-    getGameDetails(arrayOfGames);
+    // getGameDetails(arrayOfGames);
 
     console.log(":: ✓ Data pushed up to database");
     console.log("::   - details and movement for top 15 movers pushed up");
 
-    let movement = data;
-    return movement; // return hotness array of ids
+    return data; // return hotness array of ids
 
   }).then( data => {
 
@@ -75,6 +74,7 @@ let formatCrawlData = () => {
     getData.top10().then(function(top10data) {
 
       let unsorted = [],
+          top10Duplicates =[],
           top10 = [],
           movement = [];
 
@@ -91,11 +91,29 @@ let formatCrawlData = () => {
 
       // push sorted to top10 array
       for (let i = 0; i < unsorted.length; i++) {
-        top10.push(unsorted[i].bggID);
+        top10Duplicates.push(unsorted[i].bggID);
       }
 
+      // remove duplicates
+      top10 = top10Duplicates.filter(function(item, pos) {
+          return top10Duplicates.indexOf(item) == pos;
+      });
+
+      // pust top 10  game details object into array
+      for (let prop in top10data) {
+        for (let i = 0; i < top10.length; i++) {
+          if (top10data[prop].bggID === top10[i]) {
+            // set array to the whole game object
+            top10[i] = top10data[prop];
+            // console.log(`details for game ${i+1}:`, top10data[prop]);
+            break;
+          }
+        }
+      }
+
+
       // make API calls for the 10 games and push up game details
-      getGameDetails(top10);
+      // getGameDetails(top10);
 
       // format movement array
       for (let i = 0; i < movementData.length; i++) {
@@ -110,13 +128,13 @@ let formatCrawlData = () => {
 
       return getData.hotness().then(function(value) {
         // parse, cut result to first 5
-        let hotnessData = JSON.parse(value).slice(0,5);
+        let hotnessData = value.slice(0,5);
         let hotness = [];
         for (let i = 0; i < hotnessData.length; i++) {
           hotness.push(hotnessData[i].gameId);
         }
         // get game details for hotness games
-        getGameDetails(hotness);
+        // getGameDetails(hotness);
 
         // concat all game ids
         let allGameIds = data.movement.concat(hotness, data.top10);
@@ -145,43 +163,20 @@ let formatCrawlData = () => {
         for (let i = 10; i < 15; i++) {
           chartData.charts.movement.negative.push(data.movement[i]);
         }
-        chartData.charts.hotness = hotness;
+        // chartData.charts.hotness = hotness;
+        //push all hotness data for now while api is crappy
+        chartData.charts.hotness = hotnessData;
 
-
-        return {chartData, allGameIds};
+        // add crawl times
+        getCrawlTimes(chartData);
+        // push up data to Charts collection under today's date
+        // console.log("chartData:", chartData);
+        pushData(chartData, `/Charts/${today}.json`, "PATCH");
+        console.log(":: ✓ Chart data pushed up for the day");
 
         }, function(reason) {
           console.log("ERROR: Couldn't get hotness API data", reason);
       });
-
-
-    }).then( data => {
-
-      // wait 20 seconds for all data to get pushed up
-      setTimeout(function() {
-
-        let promises = [];
-
-        for (let i = 0; i < data.allGameIds.length; i++) {
-          // pull down all the games data from the database
-          let p = getData.databaseGame(data.allGameIds[i]);
-          promises.push(p);
-        }
-
-        // once the data is available,
-        Promise.all(promises).then(gameArray => {
-          for (let i = 0; i < gameArray.length; i++) {
-            // create key with game id in the games property with game data
-            data.chartData.games[gameArray[i].bggID] = gameArray[i];
-          }
-            // add crawl times
-            getCrawlTimes(data.chartData);
-            // push up data to Charts collection under today's date
-            pushData(data.chartData, `/Charts/${today}.json`, "PUT");
-            console.log(":: ✓ Chart data pushed up for the day");
-
-        });
-      }, 20000);
 
     });
   });
