bb8e9ef02e9d34592132d58a96a137731350b83d
Nathan T. Baker <nathantbaker@gmail.com>
2017-01-02 14:09:20 -0600

    three movement charts display

--- javascripts/app-logic.js
+++ javascripts/app-logic.js
@@ -27,25 +27,26 @@ getData.charts().then( unparsed => {
     );
   }
 
-  // Day Movement Chart
-  if (data.movementDay) { // check that the data's there first
-    createChart.rank(
-      "Today's Biggest Movers", // chart title
-      data, // pushes all data
-      "day", // code word for switch to know where to go in data
-      "slot2" // slot in the app to show this chart
-    );
-  }
-
   // Week Movement Chart
   if (data.movementWeek) { // check that the data's there first
     createChart.rank(
       "This Week's Biggest Movers", // chart title
       data, // pushes all data
       "week", // code word for switch to know where to go in data
+      "slot2" // slot in the app to show this chart
+    );
+  }
+
+  // Day Movement Chart
+  if (data.movementDay) { // check that the data's there first
+    createChart.rank(
+      "Today's Biggest Movers", // chart title
+      data, // pushes all data
+      "day", // code word for switch to know where to go in data
       "slot3" // slot in the app to show this chart
     );
   }
+
 
   // createChart.hotness.hotnessChart ("Most Viewed",            data, "slot2");
   // createChart.top10                ("Top 10",                 data, "slot3");
